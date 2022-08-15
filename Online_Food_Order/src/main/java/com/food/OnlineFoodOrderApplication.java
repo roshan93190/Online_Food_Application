9f55a40538a8048f99601843bcf1a2a072c44362
Niraj Kumar <n1r4jkumar@gmail.com>
2022-08-15 23:33:23 +0530

    basic application created

---
+++ Online_Food_Order/src/main/java/com/food/OnlineFoodOrderApplication.java
@@ -0,0 +1,13 @@
+package com.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderApplication.class, args);
+	}
+
+}
